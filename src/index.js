a58000c4e01fb6ebaa57a0c35052e6d13f952997
sidoroval163 <alex.sid.92@mail.ru>
2020-03-16 23:32:03 +0300

    init: arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
+exports.min = function min(array) {
+    if (
+        typeof array != "undefined" &&
+        array != null &&
+        array.length != null &&
+        array.length > 0
+    ) {
+        return Math.min.apply(null, array);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (
+        typeof array != "undefined" &&
+        array != null &&
+        array.length != null &&
+        array.length > 0
+    ) {
+        return Math.max.apply(null, array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (
+        typeof array != "undefined" &&
+        array != null &&
+        array.length != null &&
+        array.length > 0
+    ) {
+        var result = array.reduce(function(sum, current) {
+            return sum + current;
+        }, 0);
+        return result / array.length;
+    } else {
+        return 0;
+    }
+};
